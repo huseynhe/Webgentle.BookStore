d69a01b7ab89e5eaf80ca5314b6f41de5f95ad55
huseynhe <qala2009@gmail.com>
2020-12-18 10:11:21 +0400

    Add project files.

---
+++ Webgentle.BookStore/Controllers/BookController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Webgentle.BookStore.Data;
+using Webgentle.BookStore.Models;
+using Webgentle.BookStore.Repository;
+
+namespace Webgentle.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+
+        public BookController(BookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<ViewResult> GetAllBooks()
+        {
+            var data = await _bookRepository.GetBooksAsync();
+            return View(data);
+        }
+        [Route("book-details/{id}", Name = "bookDetailsRoute")]
+        public async Task<ViewResult> GetBook(int id)
+        {
+            var data = await _bookRepository.GetBooByIdAsync(id);
+            return View(data);
+        }
+        public ViewResult AddNewBook(bool isSuccess = false, int bookId = 0)
+        {
+            var model = new BookModel()
+            {
+                Language = "2",
+            };
+
+
+            //ViewBag.Language = new SelectList(GetLanguages(), "Id", "Text");
+            ViewBag.Language = new List<SelectListItem>()
+            {
+                new SelectListItem(){Text="Hindi", Value="1" },
+                new SelectListItem(){Text="English", Value="2" },
+                new SelectListItem(){Text="Dutch", Value="3" },
+                new SelectListItem(){Text="Tamil", Value="4" },
+                new SelectListItem(){Text="Urdu", Value="5" },
+                new SelectListItem(){Text="Chinese", Value="6" }
+
+            };
+            ViewBag.IsSuccess = isSuccess;
+            ViewBag.BookId = bookId;
+
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddNewBook(BookModel bookModel)
+        {
+            if (ModelState.IsValid)
+            {
+                int id = await _bookRepository.AddNewBook(bookModel);
+                if (id > 0)
+                {
+                    return RedirectToAction(nameof(AddNewBook), new { isSuccess = true, bookId = id });
+                }
+            }
+            ViewBag.Language = new List<SelectListItem>()
+            {
+                new SelectListItem(){Text="Hindi", Value="1" },
+                new SelectListItem(){Text="English", Value="2" },
+                new SelectListItem(){Text="Dutch", Value="3" },
+                new SelectListItem(){Text="Tamil", Value="4" },
+                new SelectListItem(){Text="Urdu", Value="5" },
+                new SelectListItem(){Text="Chinese", Value="6" }
+
+            };
+            return View();
+        }
+        private List<LanguageModel> GetLanguages()
+        {
+
+            return new List<LanguageModel>() {
+
+                new LanguageModel(){ Id=1,Text="Hindi"},
+                new LanguageModel(){ Id=2,Text="English"},
+                new LanguageModel(){ Id=3,Text="Dutch"},
+            };
+
+        }
+    }
+}
---
+++ Webgentle.BookStore/Repository/BookRepository.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webgentle.BookStore.Data;
+using Webgentle.BookStore.Models;
+
+namespace Webgentle.BookStore.Repository
+{
+    public class BookRepository
+    {
+        private readonly BookStoreContext _context = null;
+
+        public BookRepository(BookStoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> AddNewBook(BookModel model)
+        {
+
+            var newBook = new Books()
+            {
+                Author = model.Author,
+                CreatedOn = DateTime.UtcNow,
+                Description = model.Description,
+                Title = model.Title,
+                TotalPages = model.TotalPages,
+                UpdateOn = DateTime.UtcNow,
+
+            };
+            await _context.Books.AddAsync(newBook);
+            await _context.SaveChangesAsync();
+            return newBook.Id;
+        }
+
+        public async Task<List<BookModel>> GetBooksAsync()
+        {
+            var books = new List<BookModel>();
+            var allbooks = await _context.Books.ToListAsync();
+            if (allbooks?.Any() == true)
+            {
+                foreach (var book in allbooks)
+                {
+                    books.Add(new BookModel
+                    {
+                        Id=book.Id,
+                        Author = book.Author,
+                        Title = book.Title,
+                        Description = book.Description,
+                        TotalPages = book.TotalPages,
+
+                    });
+                }
+            }
+            return books;
+        }
+
+        public async Task<BookModel> GetBooByIdAsync(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book != null)
+            {
+                var bookDetail = new BookModel()
+                {
+                    Id = book.Id,
+                    Author = book.Author,
+                    Title = book.Title,
+                    Description = book.Description,
+                    TotalPages = book.TotalPages,
+
+                };
+                return bookDetail;
+            }
+            return null;
+        }
+
+    }
+}
